8ba9574f2f9d11ece1b2efcded1ec69a3c416736
JohnChow96927 <55785395+JohnChow96927@users.noreply.github.com>
2022-01-02 10:46:00 +0800

    str和bytes字节流的相互转换

--- Python编程进阶/day6_220102_网络编程/01-str和bytes的互相转换.py
+++ Python编程进阶/day6_220102_网络编程/01-str和bytes的互相转换.py
@@ -6,3 +6,11 @@ str和bytes的互相转换
 # bytes -> str：bytes.decode('解码方式：默认utf8')
 
 my_str = '你好！中国！'  # str
+print(type(my_str), my_str)
+
+# bytes: 字节流
+res1 = my_str.encode()  # 等价于 res1 = my_str.encode('utf8')
+print(type(res1), res1)
+
+print(res1.decode())
+# 编解码方式必须一致, 否则可能会出现乱码或报错
