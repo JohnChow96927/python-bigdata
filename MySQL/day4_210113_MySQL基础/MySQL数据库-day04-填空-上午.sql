1b4d19a9eebbc68d4d238ce73975f19ab66a1788
JohnChow96927 <55785395+JohnChow96927@users.noreply.github.com>
2022-01-13 10:33:34 +0800

    不能在HAVING和GROUP BY中使用窗口函数

--- MySQL/day4_210113_MySQL基础/MySQL数据库-day04-填空-上午.sql
+++ MySQL/day4_210113_MySQL基础/MySQL数据库-day04-填空-上午.sql
@@ -1,7 +1,8 @@
 # ============================================ 窗口函数避坑指南 ============================================
 
 DESC auction;
-SELECT * FROM auction;
+SELECT *
+FROM auction;
 
 -- 1. 不能使用窗口函数的情况
 
@@ -11,11 +12,10 @@ SELECT * FROM auction;
 -- 	id、final_price(最终成交价格)
 
 # 错误示例
-SELECT
-	id,
-	final_price
+SELECT id,
+       final_price
 FROM auction
-WHERE final_price > AVG(final_price) OVER();
+WHERE final_price > AVG(final_price) OVER ();
 
 # 正确写法(子查询)
 SELECT id,
@@ -24,7 +24,7 @@ FROM auction
 WHERE final_price > (
     SELECT AVG(final_price)
     FROM auction
-    );
+);
 
 # CTE表达式
 WITH temp_tb AS (
@@ -46,16 +46,22 @@ WHERE final_price > avg_final_price;
 -- 	country(国家)、avg(该国家所有拍卖的平均成交价)
 
 # 错误示例
-SELECT
-  country,
-  AVG(final_price)  AS `avg`
+SELECT country,
+       AVG(final_price) AS `avg`
 FROM auction
 GROUP BY country
-HAVING AVG(final_price) > AVG(final_price) OVER();
+HAVING AVG(final_price) > AVG(final_price) OVER ();
 
 
 # 正确写法(子查询)
-
+SELECT country,
+       AVG(final_price) `avg`
+FROM auction
+GROUP BY country
+HAVING AVG(final_price) > (
+    SELECT AVG(final_price)
+    FROM auction
+);
 
 
 
@@ -67,7 +73,10 @@ HAVING AVG(final_price) > AVG(final_price) OVER();
 -- 需求：将所有的拍卖信息按照浏览次数排序，并均匀分成4组，添加组号
 -- 查询结果字典：
 --  id、views(浏览次数)、quartile(分组序号)
-
+SELECT id,
+       views,
+       NTILE(4) over (ORDER BY views DESC) `quartile`
+FROM auction;
 
 
 
@@ -76,19 +85,35 @@ HAVING AVG(final_price) > AVG(final_price) OVER();
 -- 	quartile(分组序号)、min_views(当前组最小浏览量)、max_view(当前组最大浏览量)
 
 # 错误示例
-SELECT
-  NTILE(4) OVER(ORDER BY views DESC) AS `quartile`,
-  MIN(views) AS `min_views`,
-  MAX(views) AS `max_views`
+SELECT NTILE(4) OVER (ORDER BY views DESC) AS `quartile`,
+       MIN(views)                          AS `min_views`,
+       MAX(views)                          AS `max_views`
 FROM auction
-GROUP BY NTILE(4) OVER(ORDER BY views DESC);
+GROUP BY NTILE(4) OVER (ORDER BY views DESC);
 
 # 正确实现(子查询)
+SELECT `quartile`,
+       MIN(views) `min_views`,
+       MAX(views) `max_views`
+FROM (
+         SELECT views,
+                NTILE(4) over (ORDER BY views DESC) `quartile`
+         FROM auction
+     ) c
+GROUP BY `quartile`;
 
 
 # CTE公用表表达式
-
-
+WITH c AS (
+    SELECT views,
+           NTILE(4) over (ORDER BY views DESC) `quartile`
+    FROM auction
+)
+SELECT `quartile`,
+       MIN(views) `min_views`,
+       MAX(views) `max_views`
+FROM c
+GROUP BY `quartile`;
 
 # 1.2 能够使用窗口函数的情况
 
@@ -97,9 +122,11 @@ GROUP BY NTILE(4) OVER(ORDER BY views DESC);
 -- 需求：将所有的拍卖按照浏览量降序排列，并均分成4组，按照每组编号降序排列
 -- 查询结果字段：
 -- 	id(拍卖ID)、views(浏览量)、quartile(分组编号)
-
-
-
+SELECT id,
+       views,
+       NTILE(4) OVER (ORDER BY views DESC) `quartile`
+FROM auction
+ORDER BY quartile DESC;
 
 -- 情况2：窗口函数与GROUP BY一起使用
 
@@ -108,14 +135,10 @@ GROUP BY NTILE(4) OVER(ORDER BY views DESC);
 -- 	category_id(类别ID)、final_price(最终成交价格)、avg_final_price(所有拍卖平均成交价格)
 
 
-
 -- 接下来我们对上面的SQL做一个简单的调整，添加一个GROUP BY子句
 
 
-
 -- 我们再对上述窗口函数进行调整，看下这次能否正确执行
-
-
 
 
 -- 练习1
@@ -124,15 +147,11 @@ GROUP BY NTILE(4) OVER(ORDER BY views DESC);
 -- 	country(国家)、min(每组最少参与人数)、avg(所有组最少参与人数的平均值)
 
 
-
-
 -- 排序函数使用聚合函数的结果
 -- 练习2
 -- 需求：按国家进行分组，计算了每个国家的拍卖次数，再根据拍卖次数对国家进行排名
 -- 查询结果字段：
 -- 	country(国家)、count(该国家的拍卖次数)、rank(按拍卖次数的排名)
-
-
 
 
 -- 对GROUP BY分组后的数据使用PARTITION BY
--- MySQL/day4_210113_MySQL基础/README.md
+++ MySQL/day4_210113_MySQL基础/README.md
@@ -61,12 +61,94 @@
     2. 不能在HAVING子句中使用窗口函数
 
         ```mysql
+        -- 情况2：不能在HAVING子句中使用窗口函数
+        
+        -- 需求：查询出国内平均成交价格高于所有拍卖平均成交价格的国家
+        -- 查询结果字段：
+        -- 	country(国家)、avg(该国家所有拍卖的平均成交价)
+        
+        # 错误示例
+        SELECT
+          country,
+          AVG(final_price)  AS `avg`
+        FROM auction
+        GROUP BY country
+        HAVING AVG(final_price) > AVG(final_price) OVER();
+        
+        
+        # 正确写法(子查询)
+        SELECT country,
+               AVG(final_price) `avg`
+        FROM auction
+        GROUP BY country
+        HAVING AVG(final_price) > (
+            SELECT AVG(final_price)
+            FROM auction
+            );
         
         ```
 
     3. 不能在GROUP BY子句中使用窗口函数
 
+        `NTILE(X)`: 将每个分区的数据均匀分成X组, 返回每行对应的组号, 若11行分3组(除不尽)则分成4,4,3, 前面的块会多一条, 块和块之间相差不能超过1.
+
+        ```mysql
+        -- 需求：将所有的拍卖信息按照浏览次数排序，并均匀分成4组，然后计算每组的最小和最大浏览量
+        -- 查询结果字段：
+        -- 	quartile(分组序号)、min_views(当前组最小浏览量)、max_view(当前组最大浏览量)
+        
+        # 错误示例
+        SELECT NTILE(4) OVER (ORDER BY views DESC) AS `quartile`,
+               MIN(views)                          AS `min_views`,
+               MAX(views)                          AS `max_views`
+        FROM auction
+        GROUP BY NTILE(4) OVER (ORDER BY views DESC);
+        
+        # 正确实现(子查询)
+        SELECT `quartile`,
+               MIN(views) `min_views`,
+               MAX(views) `max_views`
+        FROM (
+                 SELECT views,
+                        NTILE(4) over (ORDER BY views DESC) `quartile`
+                 FROM auction
+             ) c
+        GROUP BY `quartile`;
+        
+        
+        # CTE公用表表达式
+        WITH c AS (
+            SELECT views,
+                   NTILE(4) over (ORDER BY views DESC) `quartile`
+            FROM auction
+        )
+        SELECT `quartile`,
+               MIN(views) `min_views`,
+               MAX(views) `max_views`
+        FROM c
+        GROUP BY `quartile`;
+        ```
+
 3. 能够使用窗口函数的情况
+
+    > ##### 可以在SELECT和ORDER BY中使用窗口函数
+
+    ```mysql
+    -- 情况1：在ORDER BY中使用窗口函数
+    
+    -- 需求：将所有的拍卖按照浏览量降序排列，并均分成4组，按照每组编号降序排列
+    -- 查询结果字段：
+    -- 	id(拍卖ID)、views(浏览量)、quartile(分组编号)
+    SELECT id,
+           views,
+           NTILE(4) OVER (ORDER BY views DESC) `quartile`
+    FROM auction
+    ORDER BY quartile DESC;
+    
+    
+    ```
+
+    
 
 ## II. 报表案例
 
