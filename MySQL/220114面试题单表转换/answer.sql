e4e3237730505b90a96f41549fabfc8c473cb582
JohnChow96927 <55785395+JohnChow96927@users.noreply.github.com>
2022-01-16 17:04:44 +0800

    220114 面试题

--- MySQL/220114面试题单表转换/answer.sql
+++ MySQL/220114面试题单表转换/answer.sql
@@ -1,12 +1,46 @@
-select  distinct 学号,
-        case when 学号 = 's0001' then (select 成绩 from score_220114 where 学号 = 's0001' and 科目 = '语文')
-            when 学号 = 's0002' then (select 成绩 from score_220114 where 学号 = 's0002' and 科目 = '语文')
-end as '语文',
-        case when 学号 = 's0001' then (select 成绩 from score_220114 where 学号 = 's0001' and 科目 = '数学')
-            when 学号 = 's0002' then (select 成绩 from score_220114 where 学号 = 's0002' and 科目 = '数学')
-end as '数学',
-        case when 学号 = 's0001' then (select 成绩 from score_220114 where 学号 = 's0001' and 科目 = '英语')
-            when 学号 = 's0002' then (select 成绩 from score_220114 where 学号 = 's0002' and 科目 = '英语')
-end as '英语'
-from score_220114
-;
+# 1
+SELECT 学号,
+       SUM(CASE
+               WHEN 科目 = '语文'
+                   THEN 成绩 END) `语文`,
+       SUM(CASE
+               WHEN 科目 = '数学'
+                   THEN 成绩 END) `数学`,
+       SUM(CASE
+               WHEN 科目 = '英语'
+                   THEN 成绩 END) `英语`
+
+FROM score_220114
+GROUP BY 学号;
+
+# 2
+CREATE TABLE w_score AS
+SELECT 学号,
+       SUM(CASE
+               WHEN 科目 = '语文'
+                   THEN 成绩 END) `语文`,
+       SUM(CASE
+               WHEN 科目 = '数学'
+                   THEN 成绩 END) `数学`,
+       SUM(CASE
+               WHEN 科目 = '英语'
+                   THEN 成绩 END) `英语`
+
+FROM score_220114
+GROUP BY 学号;
+
+SELECT 学号,
+       '语文' `科目`,
+       语文 `成绩`
+FROM w_score
+UNION
+SELECT 学号,
+       '数学' `科目`,
+       数学 `成绩`
+FROM w_score
+UNION
+SELECT 学号,
+       '英语' `科目`,
+       英语 `成绩`
+FROM w_score
+ORDER BY 学号;
