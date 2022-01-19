39659169946408a3c349ad3db357ff511e98bcab
JohnChow96927 <55785395+JohnChow96927@users.noreply.github.com>
2022-01-19 17:56:58 +0800

    希尔排序

--- 排序算法/ShellSort.py
+++ 排序算法/ShellSort.py
@@ -7,28 +7,25 @@ Created by JohnChow on 220119
 def shellSort(arr):
     # 待排序数组长度n
     n = len(arr)
-    # 设置初始步长
+    # 设置初始步长, //为除法向下取整
     gap = n // 2
-    # 插入排序
-    pre_index = arr[i]
-
-    return arr
-
-
-def shell_sort(arr):
-    n = len(arr)
-    # 初始步长
-    gap = n // 2
-    while gap > 0:
+    while gap >= 1:
         for i in range(gap, n):
-            # 每个步長進行插入排序
+            # 对每个步长的一组数进行插入排序
             temp = arr[i]
             j = i
-            # 插入排序
+            # 插入排序, 当j和j-gap可取且前面的数大于后面的数时
             while j >= 0 and j - gap >= 0 and arr[j - gap] > temp:
+                # 将前面的数与j位置的数交换
                 arr[j] = arr[j - gap]
+                # j向前移gap位
                 j -= gap
+            # 当前面的数(arr[j-gap])大于等于后面的数(temp)时, 将arr[j]置为temp
             arr[j] = temp
-        # 得到新的步長
+        # 更新步长
         gap = gap // 2
     return arr
+
+
+
+print(shellSort([1, 3, 5, 2, 4, 6]))
