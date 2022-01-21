c8eec1098bb51882c1330293ddfdca743038c856
JohnChow96927 <55785395+JohnChow96927@users.noreply.github.com>
2022-01-21 21:46:37 +0800

    堆排序

---
+++ 排序算法/HeapSort.py
@@ -0,0 +1,43 @@
+"""
+堆排序
+Created by JohnChow on 220121
+"""
+
+
+def buildMaxHeap(arr):
+    import math
+    for i in range(math.floor(len(arr) / 2), -1, -1):
+        heapify(arr, i)
+
+
+def heapify(arr, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < arrLen and arr[left] > arr[largest]:
+        largest = left
+    if right < arrLen and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        swap(arr, i, largest)
+        heapify(arr, largest)
+
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+
+def heapSort(arr):
+    global arrLen
+    arrLen = len(arr)
+    buildMaxHeap(arr)
+    for i in range(len(arr) - 1, 0, -1):
+        swap(arr, 0, i)
+        arrLen -= 1
+        heapify(arr, 0)
+    return arr
+
+
+if __name__ == '__main__':
+    print(heapSort([5, 1, 3, 5, 2, 4, 6]))
