d8cb651b2439b32ab4ee9bcf3e21959ff88e2764
Zhengyuan Zhou <zhouzhengyuan88@gmail.com>
2022-03-24 15:15:28 +0800

    Create 09_datasource_jdbc.py

---
+++ Spark/pyspark-lecture/pyspark-day07/main/09_datasource_jdbc.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__ == '__main__':
+    """
+    TODO:
+    SparkSQL内置数据源, 对MySQL数据库进行加载和保存
+    """
+    # 设置系统环境变量
+    os.environ['JAVA_HOME'] = '/export/server/jdk'
+    os.environ['HADOOP_HOME'] = '/export/server/hadoop'
+    os.environ['PYSPARK_PYTHON'] = '/export/server/anaconda3/bin/python3'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = '/export/server/anaconda3/bin/python3'
+
+    # 1. 获取会话实例对象-session
+    spark = SparkSession.builder \
+        .appName("Python SparkSQL Example") \
+        .master("local[*]") \
+        .getOrCreate()
+
+    # 2. 加载数据源-source
+    props = {'user': 'root',
+             'password': '123456',
+             'driver': 'com.mysql.jdbc.Driver'}
+    jdbc_df = spark.read.jdbc(
+        'jdbc:mysql://node1.itcast.cn:3306/?serverTimezone=UTC&characterEncoding=utf8&useUnicode=true',
+        'db_company.emp',
+        properties=props)
+    jdbc_df.printSchema()
+    jdbc_df.show(20, False)
+
+
+    # 3. 数据转换处理-transformation
+
+    # 4. 处理结果输出-sink
+
+    # 5. 关闭上下文对象-close
+    spark.stop()
