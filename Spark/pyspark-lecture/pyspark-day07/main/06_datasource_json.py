e4c936b80eb01f7f2a12419e4eb5b7cce7b10c32
Zhengyuan Zhou <zhouzhengyuan88@gmail.com>
2022-03-24 19:12:42 +0800

    读取, 写入json格式文件

---
+++ Spark/pyspark-lecture/pyspark-day07/main/06_datasource_json.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+if __name__ == '__main__':
+    """
+    TODO:
+    SparkSQL内置数据源，加载json数据和保存数据为json格式。
+    """
+
+    # 设置系统环境变量
+    os.environ['JAVA_HOME'] = '/export/server/jdk'
+    os.environ['HADOOP_HOME'] = '/export/server/hadoop'
+    os.environ['PYSPARK_PYTHON'] = '/export/server/anaconda3/bin/python3'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = '/export/server/anaconda3/bin/python3'
+
+    # 1. 获取会话实例对象-session
+    spark = SparkSession.builder \
+        .appName("Python SparkSQL Example") \
+        .master("local[*]") \
+        .getOrCreate()
+
+    # 2. 加载数据源-source
+    # TODO: 1. 加载JSON格式数据
+    json_df = spark.read.json('../datas/resources/people.json')
+    json_df.printSchema()
+    json_df.show(10, truncate=False)
+
+    # 3. 数据转换处理-transformation
+
+    # 4. 处理结果输出-sink
+    json_df.coalesce(1).write.mode('overwrite').json('../datas/save-json')
+
+    # 5. 关闭上下文对象-close
+    spark.stop()
