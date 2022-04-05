6ad8799f1f3a72697802a5a4c4f5e43f94bbe6ab
Zhengyuan Zhou <zhouzhengyuan88@gmail.com>
2022-04-05 21:17:18 +0800

    堆排序

--- 排序算法/HeapSort.py
+++ 排序算法/HeapSort.py
@@ -11,21 +11,17 @@ def buildMaxHeap(arr):
 
 
 def heapify(arr, i):
-    left = 2 * i + 1    # 左子节点性质
-    right = 2 * i + 2   # 右子节点性质
-    largest = i
-    if left < arrLen and arr[left] > arr[largest]:  # 若左子节点大于当前记录最大节点值
-        largest = left  # 标记位置
-    if right < arrLen and arr[right] > arr[largest]:    # 若右子节点大于当前记录最大节点值
-        largest = right # 标记
+    left_index = 2 * i + 1  # 左子节点性质
+    right_index = 2 * i + 2  # 右子节点性质
+    largest_index = i
+    if left_index < arrLen and arr[left_index] > arr[largest_index]:  # 若左子节点大于当前记录最大节点值
+        largest_index = left_index  # 标记位置
+    if right_index < arrLen and arr[right_index] > arr[largest_index]:  # 若右子节点大于当前记录最大节点值
+        largest_index = right_index  # 标记
 
-    if largest != i:    # 若最大值发生变化
-        swap(arr, i, largest)   # 将最大值与当前位置节点交换位置
-        heapify(arr, largest)   # 递归
-
-
-def swap(arr, i, j):
-    arr[i], arr[j] = arr[j], arr[i]  # 交换arr中i与j位置上的元素
+    if largest_index != i:  # 若最大值发生变化
+        arr[i], arr[largest_index] = arr[largest_index], arr[i]  # 将最大值与当前位置节点交换位置
+        heapify(arr, largest_index)  # 递归
 
 
 def heapSort(arr):
@@ -33,7 +29,7 @@ def heapSort(arr):
     arrLen = len(arr)  # 取堆中元素总数
     buildMaxHeap(arr)  # 建大顶堆
     for i in range(len(arr) - 1, 0, -1):
-        swap(arr, 0, i)  # 将堆尾与堆首元素(最大值)交换
+        arr[0], arr[i] = arr[i], arr[0]  # 将堆尾与堆首元素(最大值)交换
         arrLen -= 1  # 将当前最大值排除(更新arr)
         heapify(arr, 0)  # 将剩下的元素重新建大顶堆
     return arr
