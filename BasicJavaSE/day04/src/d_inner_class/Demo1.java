eafbeaddd3ed4c45680e13c5b12ecd770ac400d7
Zhengyuan Zhou <zhouzhengyuan88@gmail.com>
2022-04-18 22:40:55 +0800

    Day4_内部类

--- BasicJavaSE/day04/src/d_inner_class/Demo1.java
+++ BasicJavaSE/day04/src/d_inner_class/Demo1.java
@@ -1,12 +1,10 @@
-package cn.itcast.day04.d_内部类;
+package d_inner_class;
 
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class Demo01 {
+public class Demo1 {
     public static void main(String[] args) {
-        // 目标: 对数组内容排序
-        // 注意: 以后咱们的基本类型统统使用 包装类型
         Integer[] arr = {100, 20, 50, 45, 30};
 
         Arrays.sort(arr, new MyComparator());
@@ -14,8 +12,7 @@ public class Demo01 {
         System.out.println(Arrays.toString(arr));
     }
 
-    // 内部类
-    public static class MyComparator implements Comparator<Integer> {
+    private static class MyComparator implements Comparator<Integer> {
         @Override
         public int compare(Integer o1, Integer o2) {
             return o2 - o1;
