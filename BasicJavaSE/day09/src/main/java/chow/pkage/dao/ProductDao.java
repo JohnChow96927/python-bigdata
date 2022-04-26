8671b3ed39078c73f8210ab71ca3d9dca113804c
Zhengyuan Zhou <zhouzhengyuan88@gmail.com>
2022-04-26 11:53:39 +0800

    Create ProductDao.java

---
+++ BasicJavaSE/day09/src/main/java/chow/pkage/dao/ProductDao.java
@@ -0,0 +1,19 @@
+package chow.pkage.dao;
+
+public class ProductDao {
+    public void add() {
+        System.out.println("--------------------1 chow.pkage.ProductDao.add()... ...");
+    }
+
+    public void del() {
+        System.out.println("--------------------2 chow.pkage.ProductDao.del()... ...");
+    }
+
+    public void edit() {
+        System.out.println("--------------------3 chow.pkage.ProductDao.edit().... ...");
+    }
+
+    public void query() {
+        System.out.println("--------------------4 chow.pkage.ProductDao.query()... ...");
+    }
+}
