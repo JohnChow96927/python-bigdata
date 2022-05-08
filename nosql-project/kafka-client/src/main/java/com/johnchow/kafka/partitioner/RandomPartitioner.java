412caf7c6c30760324792389abd7a4b5c060e375
Zhengyuan Zhou <zhouzhengyuan88@gmail.com>
2022-05-08 18:41:15 +0800

    自定义分区器

--- nosql-project/kafka-client/src/main/java/com/johnchow/kafka/partitioner/RandomPartitioner.java
+++ nosql-project/kafka-client/src/main/java/com/johnchow/kafka/partitioner/RandomPartitioner.java
@@ -4,12 +4,20 @@ import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 
 import java.util.Map;
+import java.util.Random;
 
 public class RandomPartitioner implements Partitioner {
 
     @Override
     public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
-        return 0;
+        // 获取Topic中分区数目
+        Integer count = cluster.partitionCountForTopic(topic);
+        // 构建随机数
+        Random random = new Random();
+        // 随机生成一个分区编号
+        int partitionID = random.nextInt(count);
+        // 返回分区编号ID
+        return partitionID;
     }
 
     @Override
